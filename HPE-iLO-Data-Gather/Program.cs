5bbe7dd0cb3e4d25dccaef866f11cf0d40e166a9
Peter Upfold <upfoldp@testvalley.hants.sch.uk>
2019-05-29 14:40:19 +0100

    Add project files.

---
+++ HPE-iLO-Data-Gather/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPE_iLO_Data_Gather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
